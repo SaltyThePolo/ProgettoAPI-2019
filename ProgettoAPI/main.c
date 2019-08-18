10a4147ae2e8502deaba6b4dd2605e7b31d3bad3
mattia <tyarivi@gmail.com>
2019-08-18 15:47:35 +0200

    aggiunto cose al best

--- ProgettoAPI/main.c
+++ ProgettoAPI/main.c
@@ -1,4 +1,3 @@
-//
 //  main.c
 //  ProgettoAPI
 //
@@ -9,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct uuser {
     char name[20];
@@ -26,20 +26,34 @@ struct relation {
  // inizio con la struttura dell'albero
 
 struct node{
-    int key;
+    struct uuser data;
     struct node *father;
     struct node *left;
     struct node *right;
 };
 
-// A utility function to create a new BST node
-struct node *newNode(int item){
+struct node *newNode(struct uuser data){
     struct node *temp =  (struct node *)malloc(sizeof(struct node));
-    temp->key = item;
+    temp->data = data;
     temp->left = temp->right = NULL;
     return temp;
 }
 
+/* A utility function to insert a new node with given key in BST */
+struct node* insert(struct node* node, struct uuser data)
+{
+    /* If the tree is empty, return a new node */
+    if (node == NULL) return newNode(data);
+    
+    /* Otherwise, recur down the tree */
+    if (strcmp (data.name, node->data.name) > 0)
+        node->left  = insert(node->left, data);
+    else if (strcmp (data.name, node->data.name) > 0)
+        node->right = insert(node->right, data );
+    
+    /* return the (unchanged) node pointer */
+    return node;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
